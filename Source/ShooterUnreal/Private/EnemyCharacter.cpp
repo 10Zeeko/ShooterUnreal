76767a0a19a93beddbd06281e31bbdd36fd3d497
10Zeeko <57009593+10Zeeko@users.noreply.github.com>
2024-01-12 21:28:09 +0100

    Starting Enemy AI

--- Source/ShooterUnreal/Private/EnemyCharacter.cpp
+++ Source/ShooterUnreal/Private/EnemyCharacter.cpp
@@ -1,4 +1,6 @@
 #include "EnemyCharacter.h"
+
+#include "FPSCharacter.h"
 #include  "Components/BoxComponent.h"
 #include "Utils.h"
 #include "ShooterUnreal/Public/Utils.h"
@@ -10,7 +12,59 @@ AEnemyCharacter::AEnemyCharacter()
 	mpBoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollider"));
 	mpBoxCollision->SetupAttachment(RootComponent);
 
+	mAISight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("AISightConfig"));
+	mAISight->SightRadius = 1250.0f;
+	mAISight->LoseSightRadius = 1300.0f;
+	mAISight->PeripheralVisionAngleDegrees = 90.0f;
+	mAISight->DetectionByAffiliation.bDetectEnemies =
+		mAISight->DetectionByAffiliation.bDetectFriendlies =
+			mAISight->DetectionByAffiliation.bDetectNeutrals = true;
+	mAISight->SetMaxAge(0.1f);
+
+	mAIPerception = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
+	mAIPerception->ConfigureSense(*mAISight);
+	mAIPerception->SetDominantSense(mAISight->GetSenseImplementation());
+	mAIPerception->OnPerceptionUpdated.AddDynamic(this, &AEnemyCharacter::OnSenseCallback);
+
+	mCurrentVelocity = FVector::ZeroVector;
+	mDistanceSQRT = BIG_NUMBER;
+	
+	
 	OnTakeAnyDamage.AddDynamic(this, &AEnemyCharacter::OnHitCallback);
+}
+
+void AEnemyCharacter::OnSenseCallback(const TArray<AActor*>& aUpdateActor)
+{
+	for (const auto& UpdateActor : aUpdateActor)
+	{
+		FActorPerceptionBlueprintInfo PerceptionInfo;
+		mAIPerception->GetActorsPerception(UpdateActor, PerceptionInfo);
+
+		if (PerceptionInfo.LastSensedStimuli[0].WasSuccessfullySensed() && Cast<AFPSCharacter>(UpdateActor))
+		{			
+			SetNewRotation(UpdateActor->GetActorLocation());
+		}
+		else
+		{
+			FVector AuxDir {mSpawnPosition - GetActorLocation()};
+			AuxDir.Z = 0;
+			if (AuxDir.SizeSquared2D() > 1.0f)
+			{
+				mGoBackToBase = true;
+				SetNewRotation(mSpawnPosition);
+			}
+		}
+	}
+}
+
+void AEnemyCharacter::SetNewRotation(const FVector& aTargetRot)
+{
+	FVector NewDir = aTargetRot - GetActorLocation();
+	NewDir.Z = 0;
+	mCurrentVelocity = NewDir.GetSafeNormal() * mMoveSpeed;
+
+	mCurrentRotation = NewDir.Rotation();
+	SetActorRotation(mCurrentRotation);
 }
 
 void AEnemyCharacter::BeginPlay()
@@ -22,6 +76,27 @@ void AEnemyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (!mCurrentVelocity.IsZero())
+	{
+		mNewPosition = GetActorLocation() + mCurrentVelocity + DeltaTime;
+
+		if (mGoBackToBase)
+		{
+			double SQRT = (mNewPosition - mSpawnPosition).SizeSquared2D();
+			if (SQRT < mDistanceSQRT)
+			{
+				mDistanceSQRT = SQRT;
+			}
+			else
+			{
+				mCurrentVelocity = FVector::ZeroVector;
+				mDistanceSQRT = BIG_NUMBER;
+				mGoBackToBase = false;
+
+				SetNewRotation(GetActorForwardVector());
+			}
+		}
+	}
 }
 
 void AEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -32,6 +107,11 @@ void AEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 void AEnemyCharacter::OnHitCallback(AActor* apDamagedActor, float aDamage, const UDamageType* apDamageType,
 	AController* apInstigatedBy, AActor* apDamageCauser)
 {
+	mHealth -= aDamage;
 	ScreenD(Format1("Damage: %f", aDamage));
+	if (mHealth <= 0.0f)
+	{
+		Destroy();
+	}
 }
 
--- Source/ShooterUnreal/Public/EnemyCharacter.h
+++ Source/ShooterUnreal/Public/EnemyCharacter.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 #include "EnemyCharacter.generated.h"
 
 UCLASS()
@@ -14,6 +16,7 @@ class SHOOTERUNREAL_API AEnemyCharacter : public ACharacter
 public:
 	UFUNCTION()
 	void OnHitCallback(AActor* apDamagedActor, float aDamage, const UDamageType* apDamageType, AController* apInstigatedBy, AActor* apDamageCauser);
+	
 	AEnemyCharacter();
 	
 	virtual void Tick(float DeltaTime) override;
@@ -22,7 +25,44 @@ public:
 
 	UPROPERTY(EditDefaultsOnly)
 	class UBoxComponent* mpBoxCollision;
+
+	UPROPERTY(EditDefaultsOnly)
+	float mMoveSpeed {600.0f};
+
+	UPROPERTY(EditDefaultsOnly)
+	float mHealth {10.0f};
+
+	UPROPERTY(EditDefaultsOnly)
+	float mDamage {5.0f};
+
+#pragma region IA
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* mAIPerception;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* mAISight;
+
+	UFUNCTION()
+	void OnSenseCallback(const TArray<AActor*>& aUpdateActor);
 	
+#pragma endregion
+
 protected:
 	virtual void BeginPlay() override;
+private:
+	void SetNewRotation(const FVector& aTargetRot);
+
+	UPROPERTY(VisibleAnywhere)
+	FRotator mCurrentRotation;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector mSpawnPosition;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector mCurrentVelocity;
+
+	bool mGoBackToBase;
+	FVector mNewPosition;
+	float mDistanceSQRT;
 };
